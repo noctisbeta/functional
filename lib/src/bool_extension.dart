aa7f7f0d170bb99093916d24ae2278d988506a4a
Janez Emeršič <janez.emersic@tovarnaidej.si>
2022-11-18 04:20:26 +0100

    Added positional parameters to bool.match

--- lib/src/bool_extension.dart
+++ lib/src/bool_extension.dart
@@ -1,9 +1,9 @@
 /// Bool extension.
 extension BoolExtension on bool {
   /// Matches the value of the [bool] and returns the result of the coordinate functions.
-  B match<B>(
-    B Function() ifFalse,
-    B Function() ifTrue,
-  ) =>
+  B match<B>({
+    required B Function() ifFalse,
+    required B Function() ifTrue,
+  }) =>
       this ? ifTrue.call() : ifFalse.call();
 }
