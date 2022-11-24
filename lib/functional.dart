b5d098235a5c97e1bfa438760954ec5df78caa6b
Janez Emeršič <janez.emersic@tovarnaidej.si>
2022-11-24 03:08:46 +0100

    exported result

--- lib/functional.dart
+++ lib/functional.dart
@@ -1,8 +1,7 @@
 export 'src/bool_extension.dart';
 export 'src/either.dart';
 export 'src/option.dart';
-// Result and Either are the same thing.
-// export 'src/result.dart';
+export 'src/result.dart';
 export 'src/task.dart';
 export 'src/unit.dart';
 export 'src/util.dart';
