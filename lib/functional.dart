b67915cde4c15d29f68b9845035ba06569dc6f42
Janez Emeršič <janez.emersic@tovarnaidej.si>
2022-11-27 08:54:09 +0100

    Added extension on Task<Either>

--- lib/functional.dart
+++ lib/functional.dart
@@ -3,5 +3,6 @@ export 'src/either.dart';
 export 'src/option.dart';
 export 'src/result.dart';
 export 'src/task.dart';
+export 'src/task_either_extension.dart';
 export 'src/unit.dart';
 export 'src/util.dart';
---
+++ lib/src/task_either_extension.dart
@@ -0,0 +1,25 @@
+import 'package:functional/src/either.dart';
+import 'package:functional/src/task.dart';
+
+/// Extension for [Task<Either<L, R>].
+extension TaskEitherExtension<L, R> on Task<Either<L, R>> {
+  /// Peek either.
+  Task<Either<L, R>> peekEither(
+    void Function(L left) onLeft,
+    void Function(R right) onRight,
+  ) =>
+      Task(
+        () => run().then(
+          (either) => either.match(
+            (left) {
+              onLeft(left);
+              return either;
+            },
+            (right) {
+              onRight(right);
+              return either;
+            },
+          ),
+        ),
+      );
+}
