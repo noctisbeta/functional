3c7ec9359b6c98109bd5b746c568bcbd0a793f4c
Janez Emeršič <janez.emersic@tovarnaidej.si>
2022-11-29 09:41:15 +0100

    fix

--- lib/src/util.dart
+++ lib/src/util.dart
@@ -1,7 +1,7 @@
 import 'package:functional/src/unit.dart';
 
 /// Calls [effect] before returning [tapped].
-A tap<A, B>({required A tapped, required B Function() effect}) {
+A tap<A extends Object, B>({required A tapped, required B Function() effect}) {
   effect();
   return tapped;
 }
